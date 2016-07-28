ef6426363791bb87329c6b49b14ba6f881cbb585
Vu Tran <vu@vu-tran.com>
2016-07-28 06:40:14 -0700

    move dotfiles

--- generators/templates/index.js
+++ generators/templates/index.js
@@ -2,6 +2,10 @@ const yeoman = require('yeoman-generator');
 
 module.exports = class extends yeoman.Base {
 
+  _move(from, to) {
+    this.fs.move(this.destinationPath(from), this.destinationPath(to));
+  }
+
   writing() {
     // copies files
     this.fs.copyTpl(
@@ -9,6 +13,9 @@ module.exports = class extends yeoman.Base {
       this.destinationRoot(),
       this.options
     );
+    // move dot files
+    this._move('editorconfig', '.editorconfig');
+    this._move('gitignore', '.gitignore');
   }
 
 }
